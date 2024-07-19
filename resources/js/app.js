9ce2ef3aa321d7e8dfafafbf942f03f2109041e2
Michael Nyambura <mikethecoder12@gmail.com>
2024-07-19 21:59:04 +0300

    complete check transaction status

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,101 +1,101 @@
 import './bootstrap';
 
-// ///Get Access Token
-// document.getElementById("getAccessToken").addEventListener('click', (e) => {
-//     e.preventDefault()
-//     axios.post('/get-access-token',{})
-//     .then((response) => {
-//         document.getElementById("access_token").innerHTML = response.data
-//     })
-//     .catch((error) => {
-//         console.log("error",error)
-//     })
-// });
+///Get Access Token
+document.getElementById("getAccessToken").addEventListener('click', (e) => {
+    e.preventDefault()
+    axios.post('/get-access-token',{})
+    .then((response) => {
+        document.getElementById("access_token").innerHTML = response.data
+    })
+    .catch((error) => {
+        console.log("error",error)
+    })
+});
 
-// ///Register URLs
-// document.getElementById("registerURLs").addEventListener('click', (e) => {
-//     e.preventDefault()
-//     axios.post('/register-urls',{})
-//     .then((response) => {
-//         if (response.data.ResponseDescription) {
-//             document.getElementById("response").innerHTML = response.data.ResponseDescription
-//         }else{
-//             document.getElementById("response").innerHTML = response.data.errorMessage
-//         }
-//     })
-//     .catch((error) => {
-//         console.log("error",error)
-//     })
-// });
+///Register URLs
+document.getElementById("registerURLs").addEventListener('click', (e) => {
+    e.preventDefault()
+    axios.post('/register-urls',{})
+    .then((response) => {
+        if (response.data.ResponseDescription) {
+            document.getElementById("response").innerHTML = response.data.ResponseDescription
+        }else{
+            document.getElementById("response").innerHTML = response.data.errorMessage
+        }
+    })
+    .catch((error) => {
+        console.log("error",error)
+    })
+});
 
-// ///Simulate Transaction
-// document.getElementById("simulate").addEventListener('click', (e) => {
-//     e.preventDefault();
+///Simulate Transaction
+document.getElementById("simulate").addEventListener('click', (e) => {
+    e.preventDefault();
 
-//     const requestBody = {
-//         amount: document.getElementById("amount").value,
-//         account: document.getElementById("account").value,
-//     }
+    const requestBody = {
+        amount: document.getElementById("amount").value,
+        account: document.getElementById("account").value,
+    }
 
-//     axios.post('/simulate-transaction', requestBody)
-//     .then((response) => {
-//         if (response.data.ResponseDescription) {
-//             document.getElementById("simulate_response").innerHTML = response.data.ResponseDescription
-//         }else{
-//             document.getElementById("simulate_response").innerHTML = response.data.errorMessage
-//         }
-//     })
-//     .catch((error) => {
-//         console.log("error",error)
-//     })
-// });
+    axios.post('/simulate-transaction', requestBody)
+    .then((response) => {
+        if (response.data.ResponseDescription) {
+            document.getElementById("simulate_response").innerHTML = response.data.ResponseDescription
+        }else{
+            document.getElementById("simulate_response").innerHTML = response.data.errorMessage
+        }
+    })
+    .catch((error) => {
+        console.log("error",error)
+    })
+});
 
-// ///STK Simulation
-// document.getElementById("simulatestk").addEventListener('click', (e) => {
-//     e.preventDefault();
+///STK Simulation
+document.getElementById("simulatestk").addEventListener('click', (e) => {
+    e.preventDefault();
 
-//     const requestBody = {
-//         amount: document.getElementById("amount").value,
-//         phone: document.getElementById("phone").value,
-//         account: document.getElementById("account").value,
-//     }
+    const requestBody = {
+        amount: document.getElementById("amount").value,
+        phone: document.getElementById("phone").value,
+        account: document.getElementById("account").value,
+    }
 
-//     axios.post('/stk-simulate', requestBody)
-//     .then((response) => {
-//        if (response.data.ResponseDescription) {
-//             document.getElementById("response").innerHTML = response.data.ResponseDescription
-//         }else{
-//             document.getElementById("response").innerHTML = response.data.errorMessage
-//         }
-//     })
-//     .catch((error) => {
-//         console.log("error",error)
-//     })
-// });
+    axios.post('/stk-simulate', requestBody)
+    .then((response) => {
+       if (response.data.ResponseDescription) {
+            document.getElementById("response").innerHTML = response.data.ResponseDescription
+        }else{
+            document.getElementById("response").innerHTML = response.data.errorMessage
+        }
+    })
+    .catch((error) => {
+        console.log("error",error)
+    })
+});
 
-// ///B2C Simulation
-// document.getElementById("simulateb2c").addEventListener('click', (e) => {
-//     e.preventDefault();
+///B2C Simulation
+document.getElementById("simulateb2c").addEventListener('click', (e) => {
+    e.preventDefault();
 
-//     const requestBody = {
-//         amount: document.getElementById("amount").value,
-//         phone: document.getElementById("phone").value,
-//         occassion: document.getElementById("occassion").value,
-//         remarks: document.getElementById("remarks").value,
-//     }
+    const requestBody = {
+        amount: document.getElementById("amount").value,
+        phone: document.getElementById("phone").value,
+        occassion: document.getElementById("occassion").value,
+        remarks: document.getElementById("remarks").value,
+    }
 
-//     axios.post('/b2c-simulate', requestBody)
-//     .then((response) => {
-//        if (response.data.ResponseDescription) {
-//             document.getElementById("response").innerHTML = response.data.ResponseDescription
-//         }else{
-//             document.getElementById("response").innerHTML = response.data.errorMessage
-//         }
-//     })
-//     .catch((error) => {
-//         console.log("error",error)
-//     })
-// });
+    axios.post('/b2c-simulate', requestBody)
+    .then((response) => {
+       if (response.data.ResponseDescription) {
+            document.getElementById("response").innerHTML = response.data.ResponseDescription
+        }else{
+            document.getElementById("response").innerHTML = response.data.errorMessage
+        }
+    })
+    .catch((error) => {
+        console.log("error",error)
+    })
+});
 
 ///Transaction Status 
 document.getElementById("transactionStatus").addEventListener('click', (e) => {
